39ecddc05645b5ef9aa2418f85b23381c7b6ee50
Maxwell Bernstein <tekknolagi@gmail.com>
2019-04-01 09:30:57 -0700

    Add README/LICENSE

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import setuptools
 from distutils.core import Extension
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 simulate_fast_module = Extension(
         "simulate_fast", sources=["prisoners_problem/simulate_fast.cpp"]
 )
 
 setuptools.setup(
     name="prisoners_problem",
-    version="0.0.3",
+    version="0.0.4",
     description="Simulate the 100 prisoners problem.",
     ext_modules=[simulate_fast_module],
     url="https://github.com/tekknolagi/100prisoners",
@@ -17,5 +20,12 @@ setuptools.setup(
     install_requires=[
         'numpy==1.16.2',
         'matplotlib==3.0.3',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
